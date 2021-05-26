9339749cac618904c755d474639e7569c4074ee0
Madi <muk@alphasmart.io>
2021-05-26 23:03:05 +0600

    Сделал изменения в коде по замечаниям в комментариях

--- Урок 4. Codeception/tests/acceptance/QuickViewTheProductCest.php
+++ Урок 4. Codeception/tests/acceptance/QuickViewTheProductCest.php
@@ -2,19 +2,16 @@
 
 class QuickViewTheProductCest
 {
-    
-    // tests
-    public function tryToTest(AcceptanceTester $I)
+    public function quickViewTheProduct(AcceptanceTester $I)
     {
-        $I->amOnPage('');
-        $I->fillField('#search_query_top', 'blouse');
-        $I->click('#searchbox > button');
-        $I->moveMouseOver( '#center_column > ul > li > div > div.left-block > div > a.product_img_link > img' );
-        $I->wait(5);
-        $I->click('a[class="quick-view"]');
-        $I->see('Blouse');
+        $I->amOnPage(''); //переходим на страницу
+        $I->fillField('#search_query_top', 'blouse'); //вводим текст для поиска продукта
+        $I->click('#searchbox > button'); // подтверждаем поиск нажатием кнопки
+        $I->moveMouseOver( '#center_column > ul > li > div > div.left-block > div > a.product_img_link > img' ); //сдвигаем курсор мыши на окно с продуктом для того чтобы появилась надпись quick view
+        $I->waitForElementVisible('a[class="quick-view"]'); //пока надпись не появится
+        $I->click('a[class="quick-view"]'); //нажимаем на quick view
+        $I->switchToIFrame("fancybox-frame1622045875192"); // переключаемся на iframe
+        $I->seeElement('body[id="product"]'); // в окне quick view в первую очкеред прояверяем что объект это блузка 
+        $I->waitForText('Blouse', '#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1') //а уже потом проверяем по самому тексту продукта
     }
-        
-
-
 }
