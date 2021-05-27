36e86a5131494343fc1e9384e96dca8c8ccf91e9
Madi <muk@alphasmart.io>
2021-05-27 09:41:09 +0600

    Исправил ДЗ по комментариям

--- Урок 4. Codeception/tests/functional/CountNumberOfProductsCest.php
+++ Урок 4. Codeception/tests/functional/CountNumberOfProductsCest.php
@@ -2,16 +2,11 @@
 
 class CountNumberOfProductsCest
 {
-    
-
-    public function checkProducts(FunctionalTester $I)
+    public function checkTheNumberOfTheProducts(FunctionalTester $I)
     {
         $I->amOnPage('');
         $I->fillField('#search_query_top', 'Printed dress');
         $I->click('#searchbox > button');
         $I->seeNumberOfElements('div[class="product-container"]', 5);
     }
-        
-
-
 }
