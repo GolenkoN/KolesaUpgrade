8e3e9f16dfe0a172f4703d2bb31f3c82f9488899
Madi <muk@alphasmart.io>
2021-06-01 11:34:46 +0600

    исправил дз номер 4

--- Урок 4. Codeception/tests/acceptance/QuickViewTheProductCest.php
+++ Урок 4. Codeception/tests/acceptance/QuickViewTheProductCest.php
@@ -5,13 +5,11 @@ class QuickViewTheProductCest
     public function quickViewTheProduct(AcceptanceTester $I)
     {
         $I->amOnPage(''); //переходим на страницу
-        $I->fillField('#search_query_top', 'blouse'); //вводим текст для поиска продукта
-        $I->click('#searchbox > button'); // подтверждаем поиск нажатием кнопки
-        $I->moveMouseOver( '#center_column > ul > li > div > div.left-block > div > a.product_img_link > img' ); //сдвигаем курсор мыши на окно с продуктом для того чтобы появилась надпись quick view
-        $I->waitForElementVisible('a[class="quick-view"]'); //пока надпись не появится
-        $I->click('a[class="quick-view"]'); //нажимаем на quick view
-        $I->switchToIFrame("fancybox-frame1622045875192"); // переключаемся на iframe
-        $I->seeElement('body[id="product"]'); // в окне quick view в первую очкеред прояверяем что объект это блузка 
-        $I->waitForText('Blouse', '#product > div > div > div.pb-center-column.col-xs-12.col-sm-4 > h1') //а уже потом проверяем по самому тексту продукта
+        $I->moveMouseOver( '#homefeatured > li:nth-child(2) > div > div.left-block > div > a.product_img_link > img' ); //сдвигаем курсор мыши на окно с продуктом для того чтобы появилась надпись quick view
+        $I->waitForElementVisible('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view'); //пока надпись не появится
+        $I->click('#homefeatured > li:nth-child(2) > div > div.left-block > div > a.quick-view'); //нажимаем на quick view
+        $I->switchToIFrame('iframe.fancybox-iframe'); // переключаемся на iframe
+        $I->waitForElementVisible('body[class="product product-2 product-blouse category-7 category-blouses hide-left-column hide-right-column content_only lang_en"]'); // в окне quick view в первую очкеред прояверяем что объект это блузка 
+        $I->waitForText('Blouse', 'h1[itemprop="name"]'); //а уже потом проверяем по самому тексту продукта
     }
 }
