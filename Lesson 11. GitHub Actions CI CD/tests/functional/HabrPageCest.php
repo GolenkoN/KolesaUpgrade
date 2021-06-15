ebe4118c6e34738724ba5fd114f224046cac4a5c
Madi <muk@alphasmart.io>
2021-06-15 12:44:20 +0600

    Пайплайн создан

---
+++ Lesson 11. GitHub Actions CI CD/tests/functional/HabrPageCest.php
@@ -0,0 +1,10 @@
+<?php
+
+class HabrPageCest
+{
+    public function visitHabrPage(FunctionalTester $I)
+    {
+        $I->amOnPage('');
+        $I->click('a[href="https://habr.com/ru/flows/develop/"]');
+    }
+}
